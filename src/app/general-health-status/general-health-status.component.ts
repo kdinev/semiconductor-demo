98a14334340e747dad38373c802c13f2bad07630
Konstantin Dinev <kdinev@mail.bw.edu>
2025-04-24 18:43:53 +0300

    feat(*): updating views

---
+++ src/app/general-health-status/general-health-status.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-health-status',
+  imports: [],
+  templateUrl: './general-health-status.component.html',
+  styleUrls: ['./general-health-status.component.scss']
+})
+export class GeneralHealthStatusComponent {}
--- src/app/view-2/view-2.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-view-2',
-  imports: [],
-  templateUrl: './view-2.component.html',
-  styleUrls: ['./view-2.component.scss']
-})
-export class View2Component {}
